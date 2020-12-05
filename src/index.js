ae60497ea270c78ae739cc9ca8ecd2e9b8154cc5
Shatmanaya <sanjaykmr157@outlook.com>
2020-12-05 11:20:03 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import react from "react";
+import reactDom from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+    return(
+        <section className="booklist">
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+}
+const Book=()=>{
+    return <article className="book">
+         <Image></Image>
+         <Title/>
+         <Author/>
+         </article>
+}
+const Image=()=>(<img src="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg"alt=""/>);
+const Title=()=><h1>Rich Dad Poor Dad</h1>
+const Author=()=><h4>by Robert T. Kiyosaki</h4>
+reactDom.render(<App />,document.getElementById('root'));
